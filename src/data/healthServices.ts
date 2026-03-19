3c351fbea664b07af2a69603d1ae46d29c5899ce
Sebastián Méndez <sebasmendeza09@gmail.com>
2026-03-18 18:36:22 -0600

    feat: imagenes de SO

--- src/data/healthServices.ts
+++ src/data/healthServices.ts
@@ -22,7 +22,7 @@ export const healthServices: HealthService[] = [
       'Fortalecimiento de la cultura organizacional'
     ],
     pills: ['Prevención', 'Riesgos laborales', 'Capacitación', 'Normativa'],
-    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773450868/Logoamarillo_nboosr_tmbqhv.png'
+    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773880476/SO01_xb7vjc.jpg'
   },
   {
     id: 'asesoria-tecnica-sso',
@@ -45,7 +45,7 @@ export const healthServices: HealthService[] = [
       'Mejor toma de decisiones'
     ],
     pills: ['SSO', 'Consultoría', 'Cumplimiento', 'Prevención'],
-    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773450867/Logoletrasblancas_ciawmi_qnnxr0.png'
+    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773880476/SO04_ywdnwv.jpg'
   },
   {
     id: 'planes-sso-emergencias',
@@ -69,7 +69,7 @@ export const healthServices: HealthService[] = [
       'Continuidad operativa'
     ],
     pills: ['Planes', 'Emergencias', 'Incendios', 'Residuos'],
-    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773450868/Logoamarillo_nboosr_tmbqhv.png'
+    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773880476/SO02_kmfyfi.jpg'
   },
   {
     id: 'estudios-higiene-industrial',
@@ -92,6 +92,6 @@ export const healthServices: HealthService[] = [
       'Prevención de enfermedades profesionales'
     ],
     pills: ['Ruido', 'Iluminación', 'Higiene industrial', 'Evaluación'],
-    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773450867/Logoletrasblancas_ciawmi_qnnxr0.png'
+    image: 'https://res.cloudinary.com/deqpuhfzt/image/upload/v1773880476/SO03_v3ka9v.jpg'
   }
 ]
